89c9b2ca5167101248b0add15d9739d87982dc2a
Arthur Pimenta <92745578+Orelhals@users.noreply.github.com>
2022-08-11 18:32:15 -0300

    Create PlayScore component

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,9 @@
+import "../PlayerScore/style.css";
+
+function PlayerScore(number) {
+  return `
+    <h2 class="player-score">Player${number}:</h2>
+  `;
+}
+
+export default PlayerScore;
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import "./styles/settings/colors.css";
 import "./styles/generic/reset.css";
 import "./styles/elements/base.css";
 import CardGame from "./components/CardGame/index";
+import PlayerScore from "./components/PlayerScore/index";
 
-const $root = document.querySelector("#root");
-const $CardGameHtml = CardGame();
+const $score = document.querySelector("#score");
+const $cards = document.querySelector("#cards");
+const $CardGameHtml = CardGame() + CardGame() + CardGame();
+const $PlayerScoreHtml = PlayerScore(1) + PlayerScore(2);
 
 // Pra eu usar mais de uma vez um component é só somar +1... funciona como string
-$root.insertAdjacentHTML("beforeend", $CardGameHtml);
+$cards.insertAdjacentHTML("beforeend", $CardGameHtml + $CardGameHtml);
+$score.insertAdjacentHTML("afterbegin", $PlayerScoreHtml);
+
+// eventos
+const turnCard = document.querySelectorAll(".card-game");
+console.log(turnCard);
